8a00085331d890dc7cae26f216920826270cf9f5
Shekhar-903 <shekharmaharana1@gmail.com>
2025-12-25 11:52:46 +0530

    Update doctor.py

--- doctor.py
+++ doctor.py
@@ -1,26 +1,30 @@
 from database import connect_db
+from utils import *
 
 def add_doctor():
-    name = input("Doctor Name: ")
-    specialization = input("Specialization: ")
+    name = get_non_empty_string("Doctor Name: ")
+    specialization = get_non_empty_string("Specialization: ")
 
     conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute(
+    cur = conn.cursor()
+    cur.execute(
         "INSERT INTO doctor (name, specialization) VALUES (?, ?)",
         (name, specialization)
     )
     conn.commit()
     conn.close()
-    print("✅ Doctor added successfully")
+    print("✅ Doctor added.")
 
 def view_doctors():
     conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM doctor")
-    rows = cursor.fetchall()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM doctor")
+    rows = cur.fetchall()
     conn.close()
 
-    print("\n--- Doctor List ---")
+    if not rows:
+        print("⚠️ No doctors available.")
+        return
+
     for row in rows:
         print(row)
